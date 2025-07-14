fccc000d93e43723f9baff2887fbc039951d28d5
Basu, Anim <anim.basu@kps.com>
2025-07-14 20:42:40 +0530

    Changes in BOM

--- careAI/streamlit_app.py
+++ careAI/streamlit_app.py
@@ -5,8 +5,7 @@ def main():
     st.title("Smart BOM Analyzer")
     st.write("Welcome to the Smart BOM Analyzer. Analyze your Bill of Materials easily.")
 
-
-    st.page_link("bom_analyzer.py", label="Click Here to get Started", icon="🌎")
+    st.page_link("pages/bom_analyzer.py", label="Click Here to get Started", icon="🔍")
 
 if __name__ == '__main__':
     main()
