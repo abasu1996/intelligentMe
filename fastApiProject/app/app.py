5ec58988a420de154b84e7ca57b6a3b63e29beae
Anim Basu <animbasu1996@outlook.com>
2025-11-16 22:26:19 +0530

    fast api project and other changes in AI

---
+++ fastApiProject/app/app.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI,HTTPException
+
+app = FastAPI()
+
+all_post = {"1": {"title": "First Post", "content": "This is the content of the first post."},
+            "2": {"title": "Second Post", "content": "This is the content of the second post."}}
+
+
+@app.get("/posts")
+def get_all_posts(limit:int = None):
+    if limit:
+        return dict(list(all_post.items())[:limit])
+    return all_post
+
+@app.get("/posts/{post_id}")
+def get_post(post_id: str):
+    if post_id not in all_post:
+        raise HTTPException(status_code=404, detail="Post not found")
+    return all_post.get(post_id, {"error": "Post not found"})
+
